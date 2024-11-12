c39b22c24561ccc4631713b759016ad05fa35e65
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-11-12 12:12:51 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,21 @@ from llama_index.llms.together import TogetherLLM
 import time
 import traceback
 
+
+# Setting page layout
+st.set_page_config(
+    page_title="Literal - Self Help Interview Preperation for US Asylum Applicants",
+    page_icon="✨",
+    layout="centered",
+    initial_sidebar_state="expanded"
+)
+
+# Sidebar for API Key and User Info
+st.sidebar.header("About App")
+st.sidebar.markdown('This is an app to provide self help interview preperation for US asylum applicants created by <a href="https://ai.jdavis.xyz" target="_blank">0xjdavis</a>.', unsafe_allow_html=True)
+ 
+
+
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
@@ -519,6 +534,23 @@ def main():
     except Exception as e:
         logger.error(f"Application error: {traceback.format_exc()}")
         st.error("An unexpected error occurred. Please try again or contact support.")
+
+# Calendly
+st.sidebar.markdown("""
+    <hr />
+    <center>
+    <div style="border-radius:8px;padding:8px;background:#fff";width:100%;">
+    <img src="https://avatars.githubusercontent.com/u/98430977" alt="Oxjdavis" height="100" width="100" border="0" style="border-radius:50%"/>
+    <br />
+    <span style="height:12px;width:12px;background-color:#77e0b5;border-radius:50%;display:inline-block;"></span> <b style="color:#000000">I'm available for new projects!</b><br />
+    <a href="https://calendly.com/0xjdavis" target="_blank"><button style="background:#126ff3;color:#fff;border: 1px #126ff3 solid;border-radius:8px;padding:8px 16px;margin:10px 0">Schedule a call</button></a><br />
+    </div>
+    </center>
+    <br />
+""", unsafe_allow_html=True)
+
+# Copyright
+st.sidebar.caption("©️ Copyright 2024 J. Davis")
 
 if __name__ == "__main__":
     main()
